c79b9df76f50d2ffc47d982fc1ea2c0da69c7e40
Williamfox2012 <141017859+Programmingmonth@users.noreply.github.com>
2024-05-26 19:12:44 +0330

    first commit

---
+++ blog_project/templates/register.php
@@ -0,0 +1,26 @@
+<?php
+include 'includes/db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['$username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: login.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+?>
+<form mothod="post">
+    <label>Username:</label>
+    <input type="text" name="username" required>
+    <label>Email:</label>
+    <input type="email" name="email" required>
+    <label>Password:</label>
+    <input type="password" name="password" required>
+    <button type="submit">Register</button>
+</form>
